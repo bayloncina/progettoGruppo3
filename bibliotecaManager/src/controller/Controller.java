78f295a4912fcefb5f05bd5d2e801222b891acf1
Den-000 <dennis.a@outlook.it>
2026-04-02 16:02:47 +0200

    Upgrade controller
    Aggiunti nel controller i metodi CRUD per le risorse.
    N.B. → Per funzionare necessità di alcune metodi contenuti in Risorsa e DBConn

--- bibliotecaManager/src/controller/Controller.java
+++ bibliotecaManager/src/controller/Controller.java
@@ -1,5 +1,64 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.Risorsa;
+import service.DBConn;
+
 public class Controller {
-    
+
+    private ArrayList<Risorsa> risorse;
+    private DBConn conn;
+
+    public Controller() {
+        risorse = new ArrayList<>();
+        conn = new DBConn();
+    }
+
+    // CREATE
+    public void aggiungiRisorsa(Risorsa r) {
+        risorse.add(r);
+        conn.salvaRisorsa(r); // TODO Aggiungere in DBConn metodo slavaRisorsa(Risorsa r)
+    }
+
+    // READ (tutte)
+    public ArrayList<Risorsa> getRisorse() {
+        return risorse;
+    }
+
+    // READ (per codice)
+    public Risorsa cercaPerCodice(String codice) {
+        for (Risorsa r : risorse) {
+            if (r.getCodice().equalsIgnoreCase(codice)) { // TODO Aggiungere in Risorsa metodo getCodice()
+                return r;
+            }
+        }
+        return null;
+    }
+
+    // UPDATE
+    public boolean aggiornaRisorsa(String codice, String nuovoTitolo, int nuovoAnno) {
+        Risorsa r = cercaPerCodice(codice);
+
+        if (r != null) {
+            r.setTitolo(nuovoTitolo); // TODO Aggiungere in Risorsa metodo setTitolo(String titolo)
+            r.setAnnoPubblicazione(nuovoAnno); // TODO Aggiungere in Risorsa metodo setAnnoPubblicazione(int anno)
+
+            conn.aggiornaRisorsa(r); // TODO Aggiungere in DBConn metodo aggiornaRisorsa(Risorsa r), che dovrà aggiornare la risorsa nel database in base al codice
+            return true;
+        }
+        return false;
+    }
+
+    // DELETE
+    public boolean eliminaRisorsa(String codice) {
+        Risorsa r = cercaPerCodice(codice);
+
+        if (r != null) {
+            risorse.remove(r);
+            conn.eliminaRisorsa(codice); // TODO Aggiungere in DBConn metodo eliminaRisorsa(String codice), che dovrà eliminare la risorsa dal database in base al codice
+            return true;
+        }
+        return false;
+    }
 }
